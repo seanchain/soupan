66e7db3caea137ac23248caa5e0e1e244220fa28
Chen Sihang <seanchain2.0@gmail.com>
2015-02-25 17:37:11 +0800

    add exception handle

--- sou.rb
+++ sou.rb
@@ -9,7 +9,7 @@ class SouPan
   def initialize
     @keyword = ""
     @con = ""
-    @conv = Iconv.new "utf8", "gbk"  
+    @conv = Iconv.new "utf-8", "gbk"  
   end
   def work
     self.getURL
@@ -21,26 +21,31 @@ class SouPan
     self.request url
   end
   def request url
-    system "clear"
+    #system "clear"
     content = open(url).read
     @con = @conv.iconv content
     self.parse @con
   end
   def parse con
-    page = Nokogiri::HTML con, nil, "utf8"
-    pgn = page.css("a")
-    urlnext = pgn[pgn.length - 5]["href"]
-    urlnext = "http://209.85.228.22#{urlnext}"    
-    res = page.css("a.l")
-    res.each do |link|
-      puts link["href"]
-      puts link.text.split("_")[0]
-      puts ""
-    end
-    print "是否切换至下一页(y/n) "
-    choice = gets.chomp!
-    request urlnext if choice == "y"
-    exit   
+    begin
+      page = Nokogiri::HTML con, nil, "utf-8"
+      pgn = page.css("a")
+      urlnext = pgn[pgn.length - 5]["href"]
+      urlnext = "http://209.85.228.22#{urlnext}"    
+      res = page.css("a.l")
+      res.each do |link|
+        puts link["href"]
+        puts link.text.split("_")[0]
+        puts ""
+      end
+      print "是否切换至下一页(y/n) "
+      choice = gets.chomp!
+      request urlnext if choice == "y"
+      exit
+    rescue
+      print "Cannot find such content\n"
+      exit
+    end   
   end
 end
 
